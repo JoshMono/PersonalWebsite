f9b58bb8afd39be390d06fa044a7418d2b46f59d
Josh <Joshmono2008@outlook.com>
2025-03-08 11:59:29 +1100

    Local and Prod Debug env

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -25,7 +25,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
+
+print(f"DEBUG: {DEBUG}")
 
 
 
